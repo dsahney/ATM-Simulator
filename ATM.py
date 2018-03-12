14f5b5580dcf950be0e8c4961832c05473d5ee84
Dhruv Sahney <37184867+dsahney@users.noreply.github.com>
2018-03-12 00:05:36 -0400

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,47 +1,33 @@
 # ATM Machine Simulator
-
 import string
+import random
 
-string.ascii_lowercase # string.ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz'
-
+string.ascii_lowercase # string.ascisi_lowercase = 'abcdefghijklmnopqrstuvwxyz'
 name_initial = list(string.ascii_lowercase)
 
 question1 = 'What is the first letter of your last name (lowercase)?'
 
-if input(question1) in name_initial:
-    print('Thank you, let\'s begin the withdrawal process')
-
-else:
+while input(question1) not in name_initial:
     print('Try again from the start.')
-
+    
 question2 = 'How much money (in dollars) would you like?'
-
-# Allow the user to answer, telling us how much money they want.
-
 answer_q2 = float(input(question2))
 
-if answer_q2 > 1000.0:
-    print('That is a lot of money, please talk to a bank teller before withdrawing this amount')
-    question3 = 'Would you like to withdraw an amount of $1000 or less? Please answer by saying "yes" or "no".'
+if 0 < answer_q2 <= 1000.0:
+    print('Your request is being processed. Please pick up your money from the bottom of the machine')
+    for variable in range(1):
+        print('Thank you. Your withdrawal number is:', random.randint(1, 1000))
+elif answer_q2 > 1000.0:
+    print('That is a lot of money, please talk to a bank teller for such a withdrawal.')
+    question3 = 'Would you like to withdraw an amount of $1000 or less? Please answer: "yes" or "no".'
     answer_q3 = input(question3)
-    if answer_q3 == yes:
-        input(question1)
-    elif answer_q3 == no:
+    if answer_q3 == 'yes':
+        input(question2)
+    elif answer_q3 == 'no':
         print('Thank you, we are sorry we cannot accomodate withdrawals over $1000')
     else:
-        print('Please provide a valid answer, "yes" or "no".')
-        answer_q3 # Perhaps change with just input(question3)
-  
-elif 0 < answer_q2 <= 1000.0:
-    print('Your request is being processed. Please pick up your money from the bottom of the machine')
-
-    import random
-
-    for variable in range(1):
-    
-        print('Thank you. Your withdrawal number is:', random.randint(1, 1000))
-
+        print('Please provide a valid answer: "yes" or "no".')
+        answer_q3
 else:
-    print('You have entered an invalid amount, please try again until you enter a valid amount.')
-    while answer_q2 <= 0:
-        input(question2)
+    while float(input(question2)) <= 0:
+        print('You have entered an invalid amount, please try again until you enter a valid amount.')        
