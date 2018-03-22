1f3ac0247bd63d407c968718f1f837cffe57163d
Dhruv Sahney <37184867+dsahney@users.noreply.github.com>
2018-03-21 22:14:33 -0400

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -22,12 +22,27 @@ elif answer_q2 > 1000.0:
     question3 = 'Would you like to withdraw an amount of $1000 or less? Please answer: "yes" or "no".'
     answer_q3 = input(question3)
     if answer_q3 == 'yes':
-        input(question2)
+        while not 0 < answer_q2 <= 1000:
+            answer_q2 = float(input(question2))
+            if 0 < answer_q2 <= 1000:
+                print('Your request is being processed. Please pick up your money from the bottom of the machine')
+                for variable in range(1):
+                    print('Thank you. Your withdrawal number is:', random.randint(1, 1000))
+                    break
+            else:
+                print('Please try again')
     elif answer_q3 == 'no':
         print('Thank you, we are sorry we cannot accomodate withdrawals over $1000')
     else:
-        print('Please provide a valid answer: "yes" or "no".')
+        print('You have not entered a valid value, good bye.')
         answer_q3
 else:
-    while float(input(question2)) <= 0:
-        print('You have entered an invalid amount, please try again until you enter a valid amount.')        
+    while answer_q2 <= 0:
+        print('You have entered an invalid amount, please try again until you enter a valid amount.')
+        answer_q2 = float(input(question2))
+        if 0 < answer_q2 <= 1000:
+            print('Your request is being processed. Please pick up your money from the bottom of the machine')
+            for variable in range(1):
+                print('Thank you. Your withdrawal number is:', random.randint(1, 1000))
+        else:
+            print('Try again')
