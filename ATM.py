ef5a130453f19812c5c67e52b3759600297a727b
Dhruv Sahney <37184867+dsahney@users.noreply.github.com>
2018-03-24 19:07:31 -0400

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,9 +1,11 @@
 print('Hello, welcome to "Guess the Number"!')
+
 import string
 import random
 
 string.ascii_lowercase # string.ascisi_lowercase = 'abcdefghijklmnopqrstuvwxyz'
 name_initial = list(string.ascii_lowercase)
+
 question1 = 'Please type your name:'
 question2 = 'Please type the amount (in dollars) you would like:'
 question3 = 'Would you like to withdraw an amount of $1000 or less? Please type yes or no:'
@@ -24,8 +26,10 @@ def function1b(x):
 print('Welcome,', input(question1),'.')
 
 answer_q2 = float(input(question2))
+
 if 0 < answer_q2 <= 1000.0:
-    function1a('prompt1a') 
+    function1a('prompt1a')
+    
 elif answer_q2 > 1000.0:
     print('That is a lot of money, please talk to a bank teller for such a withdrawal.')
     answer_q3 = input(question3)
@@ -42,6 +46,7 @@ elif answer_q2 > 1000.0:
     else:
         print('You have not entered a valid value, good bye.')
         answer_q3
+        
 else:
     while answer_q2 <= 0:
         print('You have entered an invalid amount, please try again until you enter a valid amount.')
